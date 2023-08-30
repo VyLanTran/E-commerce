52154dcea1828920f26b63b25ceba0991511422c
Vy Tran <tranlanvy1203@gmail.com>
2023-08-30 07:15:23 -0400

    login, logout completed

--- website/views.py
+++ website/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def index(request):
+    # If user sends POST request, they want to log in
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+        else:
+            messages.success(
+                request, "Either username or password is incorrect. Try again")
+        return redirect('index')
+    # Otherwise, they just want to see the home page
+    else:
+        return render(request, 'index.html', {})
+
+def logout_user(request):
+    logout(request)
+    return redirect('index')
