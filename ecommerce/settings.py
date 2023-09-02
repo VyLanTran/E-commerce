8d94fb0df169a4b07dc17968dc280257c0f3d243
Vy Tran <tranlanvy1203@gmail.com>
2023-09-02 19:44:18 -0400

    prepare to deploy

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ubyjfp#s3xtibkurqzc+zyq%dby+1sg(w*sc4@t7ik)%lhq^7s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 LOGIN_URL = '/login'
 LOGIN_REDIRECT_URL = '/'
