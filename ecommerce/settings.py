3c32e4c6d378b30cc4ca6742577852d64983f674
Vy Tran <tranlanvy1203@gmail.com>
2023-09-03 07:51:52 -0400

    update settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = 'static/'
 # MEDIA_URL = 'media/'
 # MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-MEDIA_URLS ='/media/'
+MEDIA_URLS ='media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
