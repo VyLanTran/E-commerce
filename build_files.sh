7614de785a9a060b010c12a53a48d0ba997a390c
Vy Tran <tranlanvy1203@gmail.com>
2023-09-03 08:47:29 -0400

    another update

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,12 @@
 # pip install -r requirements.txt 
 # python3.9 manage.py collectstatic
 
+# echo "BUILD START"
+# python3.9 -m pip install -r requirements.txt
+# python3.9 manage.py collectstatic --noinput --clear
+# echo "BUILD END"
+
 echo "BUILD START"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+pip install -r requirements.txt 
+python3.9 manage.py collectstatic
 echo "BUILD END"
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -128,15 +128,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-# MEDIA_URL = 'media/'
-# MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-MEDIA_URLS ='media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URLS ='media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
